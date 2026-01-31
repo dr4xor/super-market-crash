44a88626d1a71bfdc0304d41ba6ed7f702a26f99
SimonBlasen <simon.yzf@t-online.de>
2026-01-31 12:25:03 +0100

    Worked on loading cart.

--- Assets/_Custom/Scripts/Cart/Items Container/CartItemsContainer.cs
+++ Assets/_Custom/Scripts/Cart/Items Container/CartItemsContainer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using ScriptableObjects;
 using UnityEngine;
 
 public class CartItemsContainer : MonoBehaviour
@@ -10,33 +11,46 @@ public class CartItemsContainer : MonoBehaviour
     [SerializeField] private float flyTime = 0.5f;
     [SerializeField] private Vector3 velocityWhenFlyingFinished = new Vector3(0f, -3f, 0f);
     [SerializeField] private string layerOfItems;
+    [SerializeField] private float timeUntilFreezeItems = 0.5f;
+    [SerializeField] private ItemTemplate[] itemTemplates;
+    [SerializeField] private float dragWhenInCart;
+    [SerializeField] private float angDragWhenInCart;
 
     private List<ItemFacade> _itemsInCart = new List<ItemFacade>();
+    private List<float> _freezeItemsIn = new List<float>();
 
     public void InstantiateAndAddItemToCart()
     {
         var goItem = Instantiate(prefabItemExample);
+        goItem.transform.localScale = Vector3.one * 0.17f;
+        goItem.Init(itemTemplates[Random.Range(0, itemTemplates.Length)]);
         goItem.transform.position = spawnOfExampleItem.position;
         AddItemToCart(goItem);
-    }   
-    
+    }
+
+
     public void AddItemToCart(ItemFacade item)
     {
-        setLayerRecursively(item.gameObject, LayerMask.NameToLayer(layerOfItems));
         item.GetComponent<Rigidbody>().isKinematic = true;
         CartFlyingItem cartFlyingItem = item.gameObject.AddComponent<CartFlyingItem>();
         cartFlyingItem.StartFlying(flyTime, yHeightFlyCurve, posOfItemToFlyTo);
-        
         cartFlyingItem.OnFlyingFinished += OnFlyingFinished;
     }
 
     private void OnFlyingFinished(CartFlyingItem cartFlyingItem)
     {
+        setLayerRecursively(cartFlyingItem.gameObject, LayerMask.NameToLayer(layerOfItems));
+
         Rigidbody rb = cartFlyingItem.GetComponent<Rigidbody>();
         rb.isKinematic = false;
+        rb.linearDamping = dragWhenInCart;
+        rb.angularDamping = angDragWhenInCart;
+        rb.mass = 0.01f;
 
         rb.linearVelocity = velocityWhenFlyingFinished;
         _itemsInCart.Add(cartFlyingItem.GetComponent<ItemFacade>());
+        _freezeItemsIn.Add(timeUntilFreezeItems);
+
 
         Destroy(cartFlyingItem);
 
@@ -45,7 +59,25 @@ public class CartItemsContainer : MonoBehaviour
 
     private void Update()
     {
-        
+        handleFreezeItems();
+    }
+
+    private void handleFreezeItems()
+    {
+        for (int i = 0; i < _freezeItemsIn.Count; i++)
+        {
+            if (_freezeItemsIn[i] > 0)
+            {
+                _freezeItemsIn[i] -= Time.deltaTime;
+
+                if (_freezeItemsIn[i] <= 0)
+                {
+                    _itemsInCart[i].transform.parent = transform;
+                    _itemsInCart[i].GetComponent<Rigidbody>().isKinematic = true;
+                    Destroy(_itemsInCart[i].GetComponent<Rigidbody>());
+                }
+            }
+        }
     }
 
     private void setLayerRecursively(GameObject go, int layer)
