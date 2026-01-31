8eb0e513146040138a67092cd2ef75def9af645c
Daniel Ruppert <niocron.games@gmail.com>
2026-01-31 18:20:32 +0100

    handle multiple shelves in range

--- Assets/_Custom/Scripts/Player/PlayerShelfInteractor.cs
+++ Assets/_Custom/Scripts/Player/PlayerShelfInteractor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DG.Tweening;
 using DG.Tweening.Core;
 using DG.Tweening.Plugins.Options;
@@ -7,8 +8,9 @@ using UnityEngine;
 public class PlayerShelfInteractor : MonoBehaviour
 {
     [SerializeField] private CartItemsContainer itemsContainer;
-    
-    private ShelfFacade _shelfInRange;
+
+    private readonly HashSet<ShelfFacade> _shelvesInRange = new ();
+    private ShelfFacade _closestShelf;
     private Player _player;
     private UI_PickupHUD _pickupHud;
     private TweenerCore<float, float, FloatOptions> _tween;
@@ -18,51 +20,85 @@ public class PlayerShelfInteractor : MonoBehaviour
         _player = GetComponent<Player>();
     }
 
+    private void Update()
+    {
+        UpdateClosestShelf();
+    }
+
+    private void UpdateClosestShelf()
+    {
+        ShelfFacade newClosest = null;
+        var closestDistance = float.MaxValue;
+
+        foreach (var shelf in _shelvesInRange)
+        {
+            if (!shelf)
+            {
+                continue;
+            }
+
+            var distance = Vector3.Distance(transform.position, shelf.transform.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                newClosest = shelf;
+            }
+        }
+
+        if (newClosest != _closestShelf)
+        {
+            _closestShelf = newClosest;
+            UpdateHUD();
+        }
+        else if (_closestShelf && !_pickupHud && _closestShelf.HasItems)
+        {
+            UpdateHUD();
+        }
+    }
+
+    private void UpdateHUD()
+    {
+        if (_pickupHud)
+        {
+            Destroy(_pickupHud.gameObject);
+            _pickupHud = null;
+        }
+
+        if (_closestShelf && _closestShelf.HasItems)
+        {
+            _pickupHud = UI_Manager.Instance.SpawnPickupHUD(_closestShelf.hudPosition, _closestShelf.itemTemplate.sprite, _player);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out ShelfFacade colliderShelf))
         {
-            _shelfInRange = colliderShelf;
-            if (_pickupHud)
-            {
-                Destroy(_pickupHud.gameObject);
-            }
-            if (_shelfInRange.HasItems)
-            {
-                _pickupHud = UI_Manager.Instance.SpawnPickupHUD(_shelfInRange.hudPosition, _shelfInRange.itemTemplate.sprite, _player);
-            }
+            _shelvesInRange.Add(colliderShelf);
         }
     }
-    
+
     private void OnTriggerExit(Collider other)
     {
         if (other.TryGetComponent(out ShelfFacade colliderShelf))
         {
-            if (colliderShelf == _shelfInRange)
-            {
-                if (_pickupHud)
-                {
-                    Destroy(_pickupHud.gameObject);
-                }
-
-                _shelfInRange = null;
-            }
+            _shelvesInRange.Remove(colliderShelf);
         }
     }
 
     public void OnInteract()
     {
-        if (_shelfInRange && _shelfInRange.HasItems)
+        if (_closestShelf && _closestShelf.HasItems)
         {
             _tween = DOTween.To(() => 0f, x => _pickupHud.SetProgress(x), 1f, 2f)
                 .SetEase(Ease.Linear)
                 .SetLink(_pickupHud.gameObject)
                 .OnComplete(() =>
                 {
-                    if (_shelfInRange.TryTakeItem(out var item))
+                    if (_closestShelf.TryTakeItem(out var item))
                     {
                         itemsContainer.AddItemToCart(item);
-                        if (_shelfInRange.HasItems == false)
+                        if (_closestShelf.HasItems == false)
                         {
                             Destroy(_pickupHud.gameObject);
                         }
